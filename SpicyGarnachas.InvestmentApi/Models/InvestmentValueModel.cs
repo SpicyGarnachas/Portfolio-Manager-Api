20b09bbcf3df7067a8f26a8c750324b39274ea7e
dmartinez31 <dmartinez31@ucol.mx>
2024-01-01 12:06:27 -0600

    InvestmentValueModel Added

---
+++ SpicyGarnachas.InvestmentApi/Models/InvestmentValueModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SpicyGarnachas.InvestmentApi.Models
+{
+    public class InvestmentValueModel
+    {
+        public int id { get; set; }
+        public int? investmentId { get; set; }
+        public string? date { get; set; }
+        public string? value { get; set; }
+    }
+}

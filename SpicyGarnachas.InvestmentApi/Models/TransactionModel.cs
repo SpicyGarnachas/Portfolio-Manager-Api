79e9b0273c901bb2be343b0b842c993c6b85dc53
dmartinez31 <dmartinez31@ucol.mx>
2024-01-01 12:13:58 -0600

    Refactoring ReturnModel to TransactionModel

--- SpicyGarnachas.InvestmentApi/Models/TransactionModel.cs
+++ SpicyGarnachas.InvestmentApi/Models/TransactionModel.cs
@@ -1,9 +1,10 @@
 ﻿namespace SpicyGarnachas.InvestmentApi.Models
 {
-    public class ReturnModel
+    public class TransactionModel
     {
         public int id { get; set; }
         public int? investmentId { get; set; }
+        public int? type { get; set; }
         public DateTime? date { get; set; }
         public int? value { get; set; }
     }

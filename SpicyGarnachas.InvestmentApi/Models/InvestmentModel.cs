2d891a747ccf52b15b82a2ac332d3aac4ea536cb
dmartinez31 <dmartinez31@ucol.mx>
2024-01-01 12:01:58 -0600

    Added InvestmentModel

---
+++ SpicyGarnachas.InvestmentApi/Models/InvestmentModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SpicyGarnachas.InvestmentApi.Models
+{
+    public class InvestmentModel
+    {
+        public int id { get; set; }
+        public int? portfolioId { get; set; }
+        public string? name { get; set; }
+        public string? description { get; set; }
+        public string? platform { get; set; }
+        public string? type { get; set; }
+        public string? sector { get; set; }
+        public string? risk { get; set; }
+        public string? liquidity  { get; set; }
+    }
+}

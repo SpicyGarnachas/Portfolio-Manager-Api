5fc04a6386a44cf3292f76a5a2042a8cae6271ad
Diego Martínez <56555056+SpicyGarnachas@users.noreply.github.com>
2024-01-05 19:23:50 +0000

    Deleted DateTime? date prop

--- SpicyGarnachas.InvestmentApi/Models/TransactionModel.cs
+++ SpicyGarnachas.InvestmentApi/Models/TransactionModel.cs
@@ -7,7 +7,6 @@
         public string? type { get; set; } // return, spend, income
         public string? description { get; set; }
         public string? category { get; set; }
-        public DateTime? date { get; set; }
         public int? value { get; set; }
         public DateTime? createdOn { get; set; }
         public DateTime? updatedOn { get; set; }

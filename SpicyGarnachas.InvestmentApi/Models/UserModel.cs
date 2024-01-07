71c22d69df01d7b85eb71fe2675124c10ae1e75a
Diego Martínez <56555056+SpicyGarnachas@users.noreply.github.com>
2024-01-07 20:40:11 +0000

    Created UserModel

---
+++ SpicyGarnachas.InvestmentApi/Models/UserModel.cs
@@ -0,0 +1,10 @@
+namespace SpicyGarnachas.InvestmentApi.Models
+{
+    public class UserModel
+    {
+        int Id { get; set; }
+        string? UserName { get; set; }
+        string? Password { get; set; }
+        
+    }
+}

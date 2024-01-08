6f25dc7d276ec857c73995e3f1c1abf9220e83e8
Diego Martínez <56555056+SpicyGarnachas@users.noreply.github.com>
2024-01-08 15:20:26 +0000

    Add image property to LoginModel and rename investmentId to referenceId in TransactionModel

--- SpicyGarnachas.InvestmentApi/Models/LoginModel.cs
+++ SpicyGarnachas.InvestmentApi/Models/LoginModel.cs
@@ -5,6 +5,7 @@ namespace SpicyGarnachas.InvestmentApi.Models
         public bool IsSuccess { get; set; }
         public string? Message { get; set; }
         public int? UserId { get; set; }
+        public byte[]? image { get; set; }
         public string? Token { get; set; }
     }
 }
--- SpicyGarnachas.InvestmentApi/Models/TransactionModel.cs
+++ SpicyGarnachas.InvestmentApi/Models/TransactionModel.cs
@@ -3,7 +3,7 @@
     public class TransactionModel
     {
         public int id { get; set; }
-        public int? investmentId { get; set; }
+        public int? referenceId { get; set; }
         public string? type { get; set; } // return, spend, income
         public string? description { get; set; }
         public string? category { get; set; } // dividend, interest, capital gain, deposit, withdrawal

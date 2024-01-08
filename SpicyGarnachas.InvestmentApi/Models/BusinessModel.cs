3249ba6d0145502bdea341d7423e8405bd421a91
Diego Martínez <56555056+SpicyGarnachas@users.noreply.github.com>
2024-01-08 14:50:31 +0000

    Update BusinessModel property name from portfolioId to referenceId

--- SpicyGarnachas.InvestmentApi/Models/BusinessModel.cs
+++ SpicyGarnachas.InvestmentApi/Models/BusinessModel.cs
@@ -3,7 +3,7 @@
     public class BusinessModel
     {
         public int id { get; set; }
-        public int? portfolioId { get; set; }
+        public int? referenceId { get; set; }
         public string? name { get; set; }
         public string? description { get; set; }
         public string? sector { get; set; } // communication services, consumer discretionary, consumer staples, energy, financials, health care, industrials, information technology, materials, real estate, utilities

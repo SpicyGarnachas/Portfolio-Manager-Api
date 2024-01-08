996aae890f18897a065436d6a8a970aec25c746d
Diego Martínez <56555056+SpicyGarnachas@users.noreply.github.com>
2024-01-08 14:55:36 +0000

    Add image and currency properties to BusinessModel and InvestmentModel

--- SpicyGarnachas.InvestmentApi/Models/BusinessModel.cs
+++ SpicyGarnachas.InvestmentApi/Models/BusinessModel.cs
@@ -8,9 +8,10 @@
         public string? description { get; set; }
         public string? sector { get; set; } // communication services, consumer discretionary, consumer staples, energy, financials, health care, industrials, information technology, materials, real estate, utilities
         public bool? isActive { get; set; }
+        public byte[]? image { get; set; }
+        public string? currency { get; set; }
         public DateTime? createdOn { get; set; }
         public DateTime? updatedOn { get; set; }
         public DateTime? inactiveDate { get; set; }
-        public byte[]? image { get; set; }
     }
 }
--- SpicyGarnachas.InvestmentApi/Models/InvestmentModel.cs
+++ SpicyGarnachas.InvestmentApi/Models/InvestmentModel.cs
@@ -11,6 +11,8 @@
         public string? sector { get; set; } // communication services, consumer discretionary, consumer staples, energy, financials, health care, industrials, information technology, materials, real estate, utilities
         public int? risk { get; set; } // 1 - low (bonds, bank, other) , 2 - medium (stock, fund, real state, collectible, other), 3 - high (crypto, business, other)
         public int? liquidity { get; set; } // 1 - money available in 1 day, 2 - money available in 1 week, 3 - money available in 1 month, 4 - money available in 1 year, 5 - money available in 5 years or more
+        public byte[]? image { get; set; }
+        public string? currency { get; set; }
         public DateTime? createdOn { get; set; }
         public DateTime? updatedOn { get; set; }
     }

9375a4841f6cbcc75e26641bbb13f2c6d2e2c685
Diego Martínez <56555056+SpicyGarnachas@users.noreply.github.com>
2024-01-08 15:03:31 +0000

    Remove date property from InvestmentValueModel

--- SpicyGarnachas.InvestmentApi/Models/InvestmentValueModel.cs
+++ SpicyGarnachas.InvestmentApi/Models/InvestmentValueModel.cs
@@ -4,7 +4,6 @@
     {
         public int id { get; set; }
         public int? investmentId { get; set; }
-        public DateTime? date { get; set; }
         public int? value { get; set; }
         public string? currency { get; set; }
         public DateTime? createdOn { get; set; }

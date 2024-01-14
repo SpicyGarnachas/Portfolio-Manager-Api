a38587c86fd777d509592cafd592c86918505f09
dmartinez31 <dmartinez31@ucol.mx>
2024-01-13 23:50:33 -0600

    Refactoring TransactionRepository Query

--- SpicyGarnachas.InvestmentApi/Repositories/TransactionRepository.cs
+++ SpicyGarnachas.InvestmentApi/Repositories/TransactionRepository.cs
@@ -23,7 +23,7 @@ namespace SpicyGarnachas.InvestmentApi.Repositories
 
                 using (MySqlConnection connection = new MySqlConnection(connectionString))
                 {
-                    string sqlQuery = "SELECT * FROM Transactions";
+                    string sqlQuery = "SELECT * FROM Transaction";
                     var transactions = await connection.QueryAsync<TransactionModel>(sqlQuery);
                     return transactions.AsList().Count > 0 ? (IsSuccess: true, transactions, string.Empty) : (IsSuccess: false, null, "Database without Transactions");
                 }

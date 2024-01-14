6fe43c8beb24705672d6242ceedd234a127b248e
dmartinez31 <dmartinez31@ucol.mx>
2024-01-13 23:51:16 -0600

    Created files UserRepository And UserService

---
+++ SpicyGarnachas.InvestmentApi/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using SpicyGarnachas.InvestmentApi.Models;
+namespace SpicyGarnachas.InvestmentApi.Repositories.Interfaces
+
+{
+    public interface IUserRepository
+    {
+        Task<(bool IsSuccess, IEnumerable<UserModel>?, string Message)> GetUserData(string username);
+        Task<(bool IsSuccess, IEnumerable<UserModel>?, string Message)> RegisterUser(UserModel user);
+    }
+}
--- SpicyGarnachas.InvestmentApi/Services/UserService.cs
+++ SpicyGarnachas.InvestmentApi/Services/UserService.cs
@@ -1,6 +1,16 @@
-﻿namespace SpicyGarnachas.InvestmentApi;
+﻿using SpicyGarnachas.InvestmentApi.Repositories.Interfaces;
+using SpicyGarnachas.InvestmentApi.Services.Interfaces;
+
+namespace SpicyGarnachas.InvestmentApi;
 
 public class UserService
 {
+    private readonly IUserRepository userRepository;
+    private readonly ILogger<UserService> logger;
 
+    public UserService(IUserRepository repository, ILogger<UserService> logger)
+    {
+        this.userRepository = repository;
+        this.logger = logger;
+    }
 }

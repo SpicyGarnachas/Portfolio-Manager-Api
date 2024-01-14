3c1d985e8880f9fe278ccd13909101d0a6f7bce6
dmartinez31 <dmartinez31@ucol.mx>
2024-01-14 14:40:32 -0600

    Added methods to login and register into the userController

--- SpicyGarnachas.InvestmentApi/Controllers/UserController.cs
+++ SpicyGarnachas.InvestmentApi/Controllers/UserController.cs
@@ -8,25 +8,27 @@ namespace SpicyGarnachas.InvestmentApi.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        private readonly ITransactionService services;
+        private readonly IUserService services;
 
-        public UserController(ITransactionService services)
+        public UserController(IUserService services)
         {
             this.services = services;
         }
 
         [HttpGet]
-        [Route("LoginByUserAndPassword")]
-        public async Task<ActionResult<LoginModel>?> LoginByUserAndPassword(string username, string password)
+        [Route("Login")]
+        public async Task<ActionResult<string>?> Login(string username, string password)
         {
-            //var (IsSuccess, Result, Message) = await services.LoginByUserAndPassword(string username, string password);
-            //return IsSuccess ? Ok(Result) : BadRequest(Message);
-            await Task.Delay(1000);
-            return new LoginModel() {
-                isSuccess = true,
-                message = "Login successful",
-                userId = 1,
-                token = "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6"};
+            var (IsSuccess, Message) = await services.Login(username, password);
+            return IsSuccess ? Ok(Message) : BadRequest(Message);
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<ActionResult<string>?> Register(UserModel user)
+        {
+            var (IsSuccess, Message) = await services.Register(user);
+            return IsSuccess ? Ok(Message) : BadRequest(Message);
         }
     }
 }
